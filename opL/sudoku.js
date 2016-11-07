17f5124919a8f0fa9af4465d71270f15cc345c7e
Jari Beguš <jari.begus@gmail.com>
2016-11-07 19:20:18 +0100

    sudoku: introduce dynamic table and random number generator

---
+++ opL/sudoku.js
@@ -0,0 +1,15 @@
+var sudokuColumn, sudokuRow;
+var out = "<table>";
+for (sudokuColumn = 0; sudokuColumn < 9; sudokuColumn++) {
+  out += "<tr>";
+  for (sudokuRow = 0; sudokuRow < 9; sudokuRow++) {
+    out += "<td>";
+    if (Math.random() < 0.3) {
+      out += Math.floor(Math.random() * 9 + 1);
+    };
+    out += "</td>"
+  };
+  out += "</tr>";
+};
+out += "</table>";
+document.getElementById("sudokuTable").innerHTML = out;
