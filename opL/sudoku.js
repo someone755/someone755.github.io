0a00005397e4e430adbda44aa8326af0edeba9f3
Jari Beguš <jari.begus@gmail.com>
2016-11-28 15:43:53 +0100

    [HTML,JS] sudoku: print time needed to generate table
    Not strictly necessary since the execution time is under 2ms. But it might be needed with one of the future exercises.

--- opL/sudoku.js
+++ opL/sudoku.js
@@ -1,18 +1,25 @@
+var timeStartScript = performance.now();
 var sudokuColumn, sudokuRow;
-var out = "<table>";
+var tableOut = "<table>";
 for (sudokuColumn = 0; sudokuColumn < 9; sudokuColumn++) {
-  out += "<tr>";
+  tableOut += "<tr>";
   for (sudokuRow = 0; sudokuRow < 9; sudokuRow++) {
-    out += "<td";
+    tableOut += "<td";
     if (Math.random() < 0.3) {
-      out += ' class="number">';
-      out += Math.floor(Math.random() * 9 + 1);
+      tableOut += ' class="number">';
+      tableOut += Math.floor(Math.random() * 9 + 1);
     } else {
-      out += ">";
+      tableOut += ">";
     }
-    out += "</td>";
+    tableOut += "</td>";
   }
-  out += "</tr>";
+  tableOut += "</tr>";
 }
-out += "</table>";
-document.getElementById("sudokuTable").innerHTML = out;
+tableOut += "</table>";
+document.getElementById("sudokuTable").innerHTML = tableOut;
+var timeEndScript = performance.now();
+var timeOut = "Page loaded in ";
+timeOut += Math.round((timeEndScript - timeStartScript)*1e6)/1e6;
+timeOut += " ms";
+document.getElementById("scriptTimer").innerHTML = timeOut;
+console.log(timeOut);
